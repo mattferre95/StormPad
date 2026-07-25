cb54f8c9970cb154b98cf066995dce6aff3ed19a
Matt Ferre <mattferre05@gmail.com>
2026-07-25 19:51:26 +0200

    docs: point website to StormPad 0.1.2
    Update the homepage download URL and the site notes to the published
    v0.1.2-beta.1 prerelease asset. DOWNLOAD_AVAILABLE stays true and the layout
    and download modal are unchanged.

--- site/script.js
+++ site/script.js
@@ -1,5 +1,5 @@
 const DOWNLOAD_URL =
-  "https://github.com/mattferre95/StormPad/releases/download/v0.1.1-beta.1/StormPad-0.1.1.dmg";
+  "https://github.com/mattferre95/StormPad/releases/download/v0.1.2-beta.1/StormPad-0.1.2.dmg";
 
 const DOWNLOAD_AVAILABLE = true;
 
